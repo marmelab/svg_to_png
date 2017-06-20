4c8805241ff70c37dbdcc674f0ec251177416770
gildas <gildas.garcia@gmail.com>
2017-06-20 14:32:20 +0200

    review

--- src/getConvertToPngScript.js
+++ src/getConvertToPngScript.js
@@ -1,5 +1,5 @@
 export default (width, height) => `
-new Promise((resolve) => {
+new Promise((resolve, reject) => {
     try {
         const svg = document.querySelector('svg');
         const svgString = new XMLSerializer().serializeToString(svg);
@@ -12,12 +12,16 @@ new Promise((resolve) => {
         const img = new Image();
         const svgBlob = new Blob([svgString], {type: "image/svg+xml;charset=utf-8"});
         const url = DOMURL.createObjectURL(svgBlob);
+
         img.onload = function() {
             ctx.drawImage(img, 0, 0);
             const png = canvas.toDataURL("image/png");
             DOMURL.revokeObjectURL(png);
             resolve(png);
         };
+
+        img.onerror = reject;
+
         img.src = url;
     } catch (error) {
         resolve(error);
